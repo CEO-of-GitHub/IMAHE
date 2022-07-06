d89ff6974ea39242bd3833fc5de3ee0972d5befa
quaaa_ <88337196+CEO-of-GitHub@users.noreply.github.com>
2022-07-06 16:55:55 +0800

    Add files via upload

---
+++ PlasticDetection2.py
@@ -0,0 +1,56 @@
+'''
+ * Python program to use contours to count the pips on the dice faces.
+ *
+ * usage: python Gladys.py <filename> <threshold>
+'''
+import cv2, sys
+
+# read command-line arguments
+filename = sys.argv[1]
+t = int(sys.argv[2])
+
+# read original image
+image = cv2.imread(filename = filename)
+
+# create binary image
+gray = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(src = gray, 
+    ksize = (5, 5), 
+    sigmaX = 0)
+
+(t, binary) = cv2.threshold(src = blur, 
+    thresh = t, 
+    maxval = 255, 
+    type = cv2.THRESH_BINARY)
+
+# find contours
+(_, contours, hierarchy) = cv2.findContours(image = binary, 
+    mode = cv2.RETR_TREE, 
+    method = cv2.CHAIN_APPROX_SIMPLE)
+
+# Count the number of pips on the dice faces.
+# Iterate through hierarchy[0], first to find the indices of dice
+# contours, then again to find pip contours.
+# WRITE YOUR CODE HERE
+
+dice = []   # list of dice contours
+pips = []   # list of pip contours
+
+# find dice contours
+for (i, c) in enumerate(hierarchy[0]):
+    if c[3] == -1:
+        dice.append(i)
+    
+# find pip contours
+for (i, c) in enumerate(hierarchy[0]):
+    if c[3] in dice:
+        pips.append(i)
+        
+print("Total die roll:", len(pips))
+image= cv2.resize(image,(200,200))
+cv2.imshow('image', image)
+cv2.waitKey()
+
+
+
